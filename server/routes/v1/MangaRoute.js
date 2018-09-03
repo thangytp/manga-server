45546ee4d1c319f7857a3c9d7c87807f12045827
Unknown <thangytp@gmail.com>
2018-09-04 00:36:44 +0700

    update manga route

--- server/routes/v1/MangaRoute.js
+++ server/routes/v1/MangaRoute.js
@@ -3,11 +3,17 @@ const app = express();
 const MangaRoute = express.Router();
 const mangaController = require('../../controller/v1/manga/manga');
 
+// Defined get data(index or listing) route
+MangaRoute.get('/', mangaController.list);
+
 // Defined store route
 MangaRoute.post('/create', mangaController.create);
 
-// Defined get data(index or listing) route
-MangaRoute.get('/', mangaController.list);
+//update manga
+MangaRoute.put('/:id', mangaController.update);
+
+//get detail manga
+MangaRoute.get('/:id', mangaController.retrieve);
 
 // Defined delete | remove | destroy route
 MangaRoute.delete('/delete/:id', mangaController.delete);
