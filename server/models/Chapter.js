fe5772aac9aa3c581709790d8b4317d5b9e5c091
Unknown <thangytp@gmail.com>
2018-09-04 00:36:35 +0700

    soft delete

--- server/models/Chapter.js
+++ server/models/Chapter.js
@@ -4,6 +4,7 @@ var Manga = require('./Manga');
 var Chapter = bookshelf.Model.extend({
 	hasTimestamps: true,
 	tableName: 'chapters',
+	soft: ['deleted_at'],
 	mangas: function () {
 		return this.belongsToMany(Manga);
 	}
--- server/models/Manga.js
+++ server/models/Manga.js
@@ -5,6 +5,7 @@ var Chapter = require('./Chapter');
 var Manga = bookshelf.Model.extend({
 	hasTimestamps: true,
 	tableName: 'mangas',
+	soft: ['deleted_at'],
 	chapters: function () {
 		return this.hasMany(Chapter);
 	}
