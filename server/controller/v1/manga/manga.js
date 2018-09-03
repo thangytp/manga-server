fdf3101aacfac41357ffa2f446e3cbae09846761
Unknown <thangytp@gmail.com>
2018-09-04 00:35:08 +0700

    update controller manga

--- server/controller/v1/manga/manga.js
+++ server/controller/v1/manga/manga.js
@@ -1,6 +1,6 @@
-const { EXISTED_CODE, SUCCESS_CODE } = require('../../../helper/ResponseCode');
-const ResponseTemplate = require('../../../helper/ResponseTemplate');
-const Manga = require('../../../models/Manga');
+import ResponseCode from '../../../helper/ResponseCode';
+import ResponseTemplate from '../../../helper/ResponseTemplate';
+import Manga from '../../../models/Manga';
 
 class MangaController {
 	async list(req, res) {
@@ -24,25 +24,19 @@ class MangaController {
 
 	async create(req, res) {
 		console.log(`Add new manga ${req.body.name}`);
-		let name = req.body.name;
-		let slug = req.body.slug;
-		let other_name = req.body.other_name;
-		let status = req.body.status;
-		let description = req.body.description;
-		let cover = req.body.cover;
-		// let {name, email, email} = {req.body.name, req.body.email, req.body.password};
+		let {name, slug, other_name, status, description, cover} = req.body;
 
 		//validate data
 		if (!name || !slug || !status || !cover) {
-			res.send(ResponseTemplate.error({
+			return res.send(ResponseTemplate.error({
 				"message": "Cannot empty",
-				'code': EXISTED_CODE
+				'code': ResponseCode.INPUT_DATA_NULL
 			}));
 		}
 
 		let mangaFind = await Manga.where({name: name}).fetch();
 		if (mangaFind) {
-			return res.status(200).json({"message": "Manga existed", 'code': EXISTED_CODE, "data": mangaFind});
+			return res.status(200).json({"message": "Manga existed", 'code': ResponseCode.EXISTED_CODE, "data": mangaFind});
 		}
 
 		let manga = new Manga({
@@ -55,44 +49,73 @@ class MangaController {
 		});
 		await manga.save()
 		.then(manga => {
-			res.status(200).json({"message": "Create manga successfully", "code": SUCCESS_CODE,"data": manga});
+			return res.status(200).json({"message": "Create manga successfully", "code": ResponseCode.SUCCESS,"data": manga});
 		})
 		.catch(err => {
 			console.log(err);
-			res.status(400).send({"message": 'Unable to create manga', "code": EXISTED_CODE, 'data': ''});
+			return res.status(400).send({"message": 'Unable to create manga', "code": ResponseCode.SERVER_INTERNAL_ERROR, 'data': ''});
+		});
+	}
+
+	async update(req, res) {
+		let id = req.params.id;
+		let manga = await Manga.where({id: id}).fetch();
+		if (!manga) {
+			return res.send(ResponseTemplate.error({"message": "Manga is not exist", 'code': ResponseCode.DATA_NOT_FOUND}));
+		}
+
+		let {name, slug, other_name, status, description, cover} = req.body;
+
+		//validate data
+		if (!name || !slug || !status || !cover) {
+			return res.send(ResponseTemplate.error({
+				"message": "Cannot empty",
+				'code': ResponseCode.INPUT_DATA_NULL
+			}));
+		}
+
+		await manga.save({name, slug, other_name, status, description, cover}, {patch: true})
+		.then(manga => {
+			return res.status(200).json({"message": "Update manga successfully", "code": ResponseCode.SUCCESS,"data": manga});
+		})
+		.catch(err => {
+			console.log(err);
+			return res.status(400).send({"message": 'Unable to update manga', "code": ResponseCode.SERVER_INTERNAL_ERROR, 'data': ''});
 		});
 	}
 
 	async retrieve(req, res) {
-		let manga = await Manga.where().fetch();
+		let id = req.params.id;
+		let manga = await Manga.where({id: id}).fetch();
 		if (!manga) {
-
+			return res.send(ResponseTemplate.error({"message": "Manga is not exist", 'code': ResponseCode.DATA_NOT_FOUND}));
 		}
 
 		return res.status(200).send(ResponseTemplate.success({
 			'message': 'Success',
-			'code': SUCCESS_CODE,
+			'code': ResponseCode.SUCCESS,
 			'data': manga
 		}));
 	}
 
 	async delete(req, res) {
-		let manga = await Manga.where().fetch();
+		let id = req.params.id;
+		let manga = await Manga.where({id: id}).fetch();
 		if (!manga) {
-
+			return res.status(200).json({"message": "Manga is not exist", 'code': ResponseCode.DATA_NOT_FOUND});
 		}
 
 		await manga.destroy()
 		.then(result => {
 			return res.status(200).send(ResponseTemplate.success({
 				'message': 'Success',
-				'code': SUCCESS_CODE,
+				'code': ResponseCode.SUCCESS,
 				'data': {}
 			}));
 		})
 		.catch(err => {
 			console.log(err);
-			res.status(400).send({"message": 'Unable to delete manga', "code": EXISTED_CODE, 'data': ''});
+			return res.status(400).send({"message": 'Unable to delete manga', "code": ResponseCode.SERVER_INTERNAL_ERROR, 'data': ''});
 		});
 	}
 }
