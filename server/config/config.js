6f7385be866cdb7aacb69200164d290f7e47673e
Thang Pham <thang.pham@seldatinc.com>
2019-04-16 18:09:49 +0700

    update code

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+let config = {
+  'secret' : 'm4ng4ytp'
+};
+// export default {config;
+let config1 = {
+    'thang' : 'thangytp'
+};
+export {config, config1};
+
--- server/routes/v1/Route.js
+++ server/routes/v1/Route.js
@@ -3,6 +3,13 @@ const Route = express.Router();
 const MangaRoute = require('./MangaRoute');
 const UserRoute = require('./UserRoute');
 import ChapterRoute from './ChapterRoute';
+import * as conf from '../../config/config';
+
+Route.use(function(req, res, next) {
+    console.log(req.originalUrl);
+    console.log(conf.config1.thang);
+    next();
+});
 
 Route.use('/manga', MangaRoute);
 Route.use('/user', UserRoute);
